55891877cfdfdd1aa084a7b097b582b2e00b968a
yb300k <harapuyo@mx4.ttcn.ne.jp>
2017-02-21 01:51:21 +0900

    fix

--- app/const.py
+++ app/const.py
@@ -6,12 +6,19 @@ POKER_MUTEX_TIMEOUT = 20
 POKER_MUTEX_KEY_PREFIX = 'MUTEX_POCKER_'
 POKER_IMAGEMAP_ELEMENT_WIDTH = 260
 POKER_IMAGEMAP_ELEMENT_HEIGHT = 263
-VOTE_MUTEX_TIMEOUT = 10
+
+JOIN_MUTEX_TIMEOUT = 3
+JOIN_MUTEX_KEY_PREFIX = 'JOIN_VOTE_'
+VOTE_MUTEX_TIMEOUT = 5
 VOTE_MUTEX_KEY_PREFIX = 'MUTEX_VOTE_'
+
 POKER_IMAGE_FILENAME = 'pp-{0}.png'
 IMG_PATH = os.path.join(os.path.dirname(__file__), 'static', 'planning_poker')
 TMP_ROOT_PATH = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 BG_FILE_PATH = os.path.join(os.path.dirname(__file__), 'static', 'planning_poker', 'vote_background.png')
-MESSAGE_END_POKER = '#{0}のポーカーは終了してまーす'
-MESSAGE_INVALID_VOTE = '#{0}のポーカーはないですよ。手入力した？'
+MESSAGE_END_POKER = '#{0}の投票は終了してまーす'
+MESSAGE_INVALID_VOTE = '#{0}の投票はないですよ。手入力した？'
 
+HEROKU_SERVER_URL = 'https://fivesecvote.herokuapp.com/'
+
+RESULT_DISPLAY_TIMEOUT = 1
