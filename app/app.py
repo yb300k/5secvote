424959cd0a8f74fb19f141f1473db83dbd3c69d6
yb300k <harapuyo@mx4.ttcn.ne.jp>
2017-02-21 01:31:34 +0900

    first

--- app/app.py
+++ app/app.py
@@ -22,6 +22,7 @@ from linebot.models import (
     TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
     ButtonsTemplate, URITemplateAction, PostbackTemplateAction,
     CarouselTemplate, CarouselColumn, PostbackEvent,
+    UnfollowEvent, FollowEvent,ImageSendMessage,
     ImagemapSendMessage, MessageImagemapAction, BaseSize, ImagemapArea
 )
 
@@ -51,6 +52,13 @@ def callback():
         abort(400)
     return 'OK'
 
+@app.route('/images/button/<size>', methods=['GET'])
+def download_imageam(size):
+    filename = AM_IMAGE_FILENAME.format(size)
+    return send_from_directory(os.path.join(app.root_path, 'static', 'ambutton'),
+            filename)
+
+
 @app.route('/images/tmp/<number>/<filename>', methods=['GET'])
 def download_result(number, filename):
     return send_from_directory(os.path.join(app.root_path, 'static', 'tmp', number), filename)
@@ -61,56 +69,111 @@ def download_imagemap(size):
     return send_from_directory(os.path.join(app.root_path, 'static', 'planning_poker'),
             filename)
 
+@handler.add(FollowEvent)
+def handle_follow(event):
+#友達追加イベント、ここでredisへの登録を行う
+#    sourceId = getSourceId(event.source)
+#    profile = line_bot_api.get_profile(sourceId)
+#    display_name = getUtfName(profile)
+
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text='こんにちわ\uD83D\uDE04\n'+
+        'みんなでせーのでタイミングを合わせて参加ボタンを押してね\uD83D\uDE03'))
+
+    line_bot_api.push_message(
+        sourceId,generateJoinButton())
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     text = event.message.text
     sourceId = getSourceId(event.source)
     matcher = re.match(r'^#(\d+) (.+)', text)
 
-    if text == 'プラポ':
-        poker_mutex = Mutex(redis, POKER_MUTEX_KEY_PREFIX+ sourceId)
-        poker_mutex.lock()
-        if poker_mutex.is_lock():
-            number = str(redis.incr(sourceId)).encode('utf-8')
-            line_bot_api.reply_message(
-               event.reply_token,
-               generate_planning_poker_message(number))
-            time.sleep(POKER_MUTEX_TIMEOUT)
-            if poker_mutex.is_lock():
-                poker_mutex.unlock()
+    if text == 'join':#メンバ集め・・・今後要検討
+        join_mutex = Mutex(redis, JOIN_MUTEX_KEY_PREFIX+ sourceId)
+        join_mutex.lock()
+        if join_mutex.islock():
+            number = str(redis.incr('maxVoteKey')).encode('utf-8')
+            time.sleep(JOIN_MUTEX_TIMEOUT)
+            if redis.sismember(number,sourceId) == 0:
+                redis.sadd(number,sourceId)
+                redis.hset(sourceId,'current',number)
+
+            push_all(number,generate_planning_poker_message)
+        else:
+            number = str(redis.get('maxVoteKey').encode('utf-8'))
+            if redis.sismember(number,sourceId) == 0:
+                redis.sadd(number,sourceId)
+                redis.hset(sourceId,'current',number)
+    elif text == 'add':
+        pass
+
     elif matcher is not None:
         number = matcher.group(1)
         value = matcher.group(2)
         current = redis.get(sourceId).encode('utf-8')
-        vote_key = sourceId + number 
+        vote_key = 'res_' + number
         status = redis.hget(vote_key, 'status')
-        if status is None:
-            if number != current:
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextMessage(text=MESSAGE_INVALID_VOTE.format(number)))
-                return
-            poker_mutex = Mutex(redis, POKER_MUTEX_KEY_PREFIX+ sourceId)
-            vote_mutex = Mutex(redis, VOTE_MUTEX_KEY_PREFIX  + sourceId)
-            location = mapping.keys()[mapping.values().index(value)]
-            vote_mutex.lock()
-            if vote_mutex.is_lock():
-                time.sleep(VOTE_MUTEX_TIMEOUT)
-                redis.hincrby(vote_key, location)
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    genenate_voting_result_message(vote_key)
-                )
-                redis.hset(vote_key, 'status', 'complete')
-                vote_mutex.unlock()
-                poker_mutex.release()
-            else:
-                redis.hincrby(vote_key, location)
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextMessage(text=MESSAGE_END_POKER.format(number)))
 
+        poker_mutex = Mutex(redis, POKER_MUTEX_KEY_PREFIX+ sourceId)
+        vote_mutex = Mutex(redis, VOTE_MUTEX_KEY_PREFIX  + sourceId)
+        location = mapping.keys()[mapping.values().index(value)]
+        vote_mutex.lock()
+        if vote_mutex.is_lock():
+            time.sleep(VOTE_MUTEX_TIMEOUT)
+            redis.hincrby(vote_key, location)
+
+            push_result_message(number)
+
+            res_list = redis.hkeys(vote_key)
+            for value in res_list.itervalues():
+                 redis.hdel(vote_key,value)
+
+            vote_mutex.unlock()
+            poker_mutex.release()
+        else:
+            redis.hincrby(vote_key, location)
+
+def push_result_message(vote_key):
+    push_all(vote_key,
+        TextSendMessage(text='3位は))
+    time.sleep(RESULT_DISPLAY_TIMEOUT)
+    push_all(vote_key,
+        TextSendMessage(text='2位は'))
+    time.sleep(RESULT_DISPLAY_TIMEOUT)
+    push_all(vote_key,
+        TextSendMessage(text='1位は・・・・'))
+    time.sleep(RESULT_DISPLAY_TIMEOUT)
+    push_all(vote_key,
+        TextSendMessage(text='でした！'))
+
+def push_all(vote_key,message):
+    data = redis.hgetall(vote_key)
+    for value in data.itervalues():
+        line_bot_api.push_message(value,message)
+
+def generateJoinButtons():
+    message = ImagemapSendMessage(
+        base_url= HEROKU_SERVER_URL + 'images/joinbutton',
+        alt_text='join',
+        base_size=BaseSize(height=178, width=1040))
+    actions=[]
+    actions.append(MessageImagemapAction(
+        text = 'join',
+        area=ImagemapArea(
+            x=0,
+            y=0,
+            width = BUTTON_ELEMENT_WIDTH,
+            height = BUTTON_ELEMENT_HEIGHT)))
+    actions.append(MessageImagemapAction(
+        text = 'add',
+        area=ImagemapArea(
+            x=BUTTON_ELEMENT_WIDTH,
+            y=0,
+            width = BUTTON_ELEMENT_WIDTH * 2,
+            height = BUTTON_ELEMENT_HEIGHT)))
+    message.actions = actions
+    return message
 
 def genenate_voting_result_message(key):
     data = redis.hgetall(key)
@@ -147,4 +210,3 @@ def generate_planning_poker_message(number):
             location+=1
     message.actions = actions
     return message
-
