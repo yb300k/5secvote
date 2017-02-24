a03aed3c3b2c554a46f853c3164ae77f233f9299
yb300k <harapuyo@mx4.ttcn.ne.jp>
2017-02-25 04:04:05 +0900

    imagemap

--- app/app.py
+++ app/app.py
@@ -131,7 +131,7 @@ def handle_text_message(event):
 
         if redis.hget(sourceId,'voted') == 'Y':
             line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(text='すでに投票済です・・結果集計をお待ちください')
+                event.reply_token, TextSendMessage(text='すでに投票済です・・結果集計をお待ちください'))
             return
         else:
             redis.hset(sourceId,'voted','Y')
