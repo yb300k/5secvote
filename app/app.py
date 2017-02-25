b8314faf3157655413a9af44a62340132b283a0d
yb300k <harapuyo@mx4.ttcn.ne.jp>
2017-02-26 02:52:54 +0900

    fix

--- app/app.py
+++ app/app.py
@@ -324,7 +324,7 @@ def generate_member_list_from_value(result_dict,objvalue,vote_num):
         if value == objvalue:
             ret_str += getNameFromNum(vote_num,key)+'さん '
             count += 1
-    ret_str += '('+objvalue'票)でした！'
+    ret_str += '(' + objvalue + '票)でした！'
 
     return (ret_str,count)
 
